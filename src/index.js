03f33f18cf15b92b5558c1088dc2fa66245e3624
Ferreira-Orlann <ferreira.orlann@gmail.com>
2024-10-07 13:07:36 +0200

    XX

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import mongoose from "mongoose"
 import 'dotenv/config'
 import { UserRouter } from "./express/routers/user.js"
 import { initialize } from "express-openapi";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDoc from "../doc/swagger_doc.json" assert { type: 'json' };
 
 const app = express()
 
+app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDoc));
+
 mongoose.connect(process.env.DB_URL).then(() => {
     console.log("Database Connected:")
-    initialize({
-        app,
-        docsPath: "/doc",
-        paths: "./doc/specs",
-        exposeApiDocs: (process.env.ENV_MODE !== "production"),
-        apiDoc: "./doc/api-definition-base.yml"
-    });
 
     app.listen(process.env.EXPRESS_PORT, (err) => {
         console.log("Server running at http://localhost:3000/ !")
