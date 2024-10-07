271d1c7ce997ee4923f23ceacd78e39ebed6f430
Ferreira-Orlann <ferreira.orlann@gmail.com>
2024-10-07 09:30:05 +0200

    Init n°2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("test")
+import express from "express"
+
+const PORT = 3000
+const ACCEPT_TOKEN = "P46PHI65EFmkyUTqt2b00IUC3hOIl3k8Ogiwu0qAPm7MbOdqekdOPjdiqhfiS4jP"
+const DB_URL = "mongodb+srv://admin:admin@cluster-supinfo.c6xyt.mongodb.net/"
+
+const app = express()
+
