6c0f171537a92515673a93e4f080931daaed5b8c
Ferreira-Orlann <ferreira.orlann@gmail.com>
2024-10-07 11:38:18 +0200

    Separate User Router and User Controller's code

--- src/express/controllers/user.js
+++ src/express/controllers/user.js
@@ -1,19 +1,83 @@
+import jsonwebtoken from "jsonwebtoken";
+import { User } from "../../database/models/user.js";
+import { hashSync, genSaltSync, hash } from "bcrypt";
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 export function UserRegister(req, res) {
-    
+    let email = req.body.email
+    if (!emailRegex.test(email)) {
+        res.status(400).send("Not an Email")
+        return   
+    }
+    let username = req.body.username
+    let password = req.body.password
+    if (email === undefined 
+        || username === undefined
+        || password === undefined) {
+        res.status(400).send("Error")
+        return
+    }
+    let user = new User({
+        username: username,
+        email: email,
+        hashedPassword: hashSync(password, process.env.HASH_SALT)
+    })
+    user.save().then(() => {
+        res.status(200).json(user)
+    }).catch((err) => {
+        console.log("User Register Error:", err)
+        res.status(500).send("User Register Error: " + err)
+    })
 }
 
 export function UserLogin(req, res) {
-
+    const password = req.body.password
+    const email = req.body.email
+    if (email === undefined || password === undefined) {
+        res.status(400).send("Email or Password undefined")
+        return
+    }
+    User.findOne({email: email, hashedPassword: hashSync(password, process.env.HASH_SALT)}).then((user) => {
+        console.log(user)
+        const accessToken = jsonwebtoken.sign(user.id, process.env.JWT_KEY);
+        res.status(200).json({ accessToken});
+    }).catch((err) => {
+        res.status(500).send("Login Error: " + err)
+    })
 } 
 
 export function UserGet(req, res) {
-
+    User.findOne({_id: req.params.id}).then((user) => {
+        console.log("Deleted:",user.IsDeleted())
+        if (user.IsDeleted()) {
+            res.status(400).send("User Deleted")
+            return
+        }
+        res.status(200).json(user)
+    }).catch((err) => {
+        res.status(500).send("User Get Error: " + err)
+    })
 }
 
 export function UserPut(res, req) {
-
+    User.findOneAndUpdate({_id: req.params.id}, req.body, {
+        returnOriginal: false
+    }).then((user) => {
+        if (user.IsDeleted()) {
+            res.status(400).send("User Deleted")
+            return
+        }
+        res.status(200).json(user)
+    }).catch((err) => {
+        res.status(500).send("User Put Error: " + err)
+    })
 }
 
 export function UserDelete(res, req) {
-
+    User.findOneAndUpdate({_id: req.params.id}, {deletedAt: Date.now()}).then((user) => {
+        res.status(200).send("Ok")
+    }).catch((err) => {
+        res.status(500).send("User Delete Error: " + err)
+    })
 }
--- src/express/routers/user.js
+++ src/express/routers/user.js
@@ -1,89 +1,16 @@
 import express from "express"
 import { UserRegister, UserLogin, UserGet, UserPut, UserDelete } from "../controllers/user.js"
-import { User } from "../../database/models/user.js";
-import { hashSync, genSaltSync, hash } from "bcrypt";
 import { JWTAuthMiddleware } from "../middlewares/jwtauth.js";
-import jsonwebtoken from "jsonwebtoken";
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const UserRouter = express.Router()
 
 UserRouter.use(express.json())
 
-UserRouter.post("/register", (req, res) => {
-    let email = req.body.email
-    if (!emailRegex.test(email)) {
-        res.status(400).send("Not an Email")
-        return   
-    }
-    let username = req.body.username
-    let password = req.body.password
-    if (email === undefined 
-        || username === undefined
-        || password === undefined) {
-        res.status(400).send("Error")
-        return
-    }
-    let user = new User({
-        username: username,
-        email: email,
-        hashedPassword: hashSync(password, process.env.HASH_SALT)
-    })
-    user.save().then(() => {
-        res.status(200).json(user)
-    }).catch((err) => {
-        console.log("User Register Error:", err)
-        res.status(500).send("User Register Error: " + err)
-    })
-})
-UserRouter.post("/login", (req, res) => {
-    const password = req.body.password
-    const email = req.body.email
-    if (email === undefined || password === undefined) {
-        res.status(400).send("Email or Password undefined")
-        return
-    }
-    User.findOne({email: email, hashedPassword: hashSync(password, process.env.HASH_SALT)}).then((user) => {
-        console.log(user)
-        const accessToken = jsonwebtoken.sign(user.id, process.env.JWT_KEY);
-        res.status(200).json({ accessToken});
-    }).catch((err) => {
-        res.status(500).send("Login Error: " + err)
-    })
-})
+UserRouter.post("/register", UserRegister)
+UserRouter.post("/login", UserLogin)
 
 UserRouter.use(JWTAuthMiddleware)
 
-UserRouter.get("/:id", (req, res) => {
-    User.findOne({_id: req.params.id}).then((user) => {
-        console.log("Deleted:",user.IsDeleted())
-        if (user.IsDeleted()) {
-            res.status(400).send("User Deleted")
-            return
-        }
-        res.status(200).json(user)
-    }).catch((err) => {
-        res.status(500).send("User Get Error: " + err)
-    })
-})
-UserRouter.put("/:id", (req, res) => {
-    User.findOneAndUpdate({_id: req.params.id}, req.body, {
-        returnOriginal: false
-    }).then((user) => {
-        if (user.IsDeleted()) {
-            res.status(400).send("User Deleted")
-            return
-        }
-        res.status(200).json(user)
-    }).catch((err) => {
-        res.status(500).send("User Put Error: " + err)
-    })
-})
-
-UserRouter.delete("/:id", (req, res) => {
-    User.findOneAndUpdate({_id: req.params.id}, {deletedAt: Date.now()}).then((user) => {
-        res.status(200).send("Ok")
-    }).catch((err) => {
-        res.status(500).send("User Delete Error: " + err)
-    })
-})
+UserRouter.get("/:id", UserGet)
+UserRouter.put("/:id", UserPut)
+UserRouter.delete("/:id", UserDelete)
