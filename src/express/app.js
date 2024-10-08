2ba40b16c1d7588b551a7fe734685cbf334dd1e5
Dstar1er <159792307+Dstar1err@users.noreply.github.com>
2024-10-08 13:53:47 +0200

    update

--- src/express/app.js
+++ src/express/app.js
@@ -30,9 +30,13 @@ const io = new Server(server)
 
 io.on("connection",  (socket) => {
     console.log("Made socket connection")
+
     socket.on("chat", (message) => {
         console.log("message dd: " , message)
+
+        io.emit("chat", message)
     })
+
 })
 
 server.listen(3001)
